d73b90a065e6ddd83e74c896e2a95809407d4bd3
Kilerd <blove694@gmail.com>
2019-08-15 15:17:41 +0800

    feat: posts api

--- server/src/routers/post.rs
+++ server/src/routers/post.rs
@@ -11,17 +11,6 @@ use tera::Context;
 
 #[get("")]
 pub fn show_posts(data: Data<AppData>) -> impl Responder {
-    //    let posts = Post::get_all_posts(&data.postgres());
-    //    let mut posts_group_by_day: BTreeMap<NaiveDate, Vec<(Post, Option<Blog>)>> = BTreeMap::new();
-    //    for x in posts {
-    //        let date = x.0.create_at.date().naive_utc();
-    //        posts_group_by_day.entry(date).or_insert(vec![]).push(x);
-    //    }
-    //    let mut post_iter: Vec<(&NaiveDate, &Vec<(Post, Option<Blog>)>)> =
-    //        posts_group_by_day.iter().collect();
-    //    let mut context = Context::new();
-    //    context.insert("posts", &post_iter);
-    //    AppResponder::html(data.render("posts.html", &context))
     let mut ret = vec![];
     for item in Post::get_all_posts(&data.postgres()) {
         ret.push(item.0);
